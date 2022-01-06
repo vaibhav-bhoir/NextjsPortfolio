6bbc1a8b2214d454cfd1868d6e5473efd4f70489
vaibhav-bhoir <bhoirvaibhav90@gmail.com>
2022-01-06 13:34:51 +0530

    initial commit

--- components/experience.data.js
+++ components/experience.data.js
@@ -17,6 +17,14 @@ const EXP_DATA = [
     },
     {
         id: 3,
+        name: 'Hero Electric',
+        post: 'Associate Developer',
+        location: 'Mirum India pvt ltd',
+        desc: 'Managed backend data with the help of wordpress CMS.',
+        date: 'Oct 2021 - DEC 2021'
+    },
+    {
+        id: 4,
         name: 'Rohan Corporation',
         post: 'Associate Developer',
         location: 'Mirum India pvt ltd',
