a85e719f6340d4a4a99e8f73edb04b31d7bf56ad
vaibhav-bhoir <bhoirvaibhav90@gmail.com>
2024-05-05 20:27:28 +0530

    feat: skill fetch from contentful cms

--- components/Circle.tsx
+++ components/Circle.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 interface CircleProps {
   data: {
-    name: string;
-    imageSrc: string;
+    title: string;
+    image_url: string;
   };
 }
 
 const Circle: React.FC<CircleProps> = ({ data }) => {
-  const { name, imageSrc } = data;
+  const { title, image_url } = data;
 
   return (
     <div
@@ -18,9 +18,9 @@ const Circle: React.FC<CircleProps> = ({ data }) => {
       className="shadow-shadow bg-main-bg w-24 h-24 md:w-28 md:h-28 rounded-full flex flex-col justify-center items-center"
     >
       <div className="mt-1">
-        <Image src={imageSrc} alt="icon" width={40} height={40} />
+        <Image src={image_url} alt="icon" width={40} height={40} />
       </div>
-      <h1 className="text-md lg:text-base font-medium text-primary">{name}</h1>
+      <h1 className="text-md lg:text-base font-medium text-primary">{title}</h1>
     </div>
   );
 };
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import Head from 'next/head';
-import Circle from '../components/Circle';
-import SKILL_DATA from '../public/data/skill.data';
 import CustomHeading from '../components/CustomHeading';
 import CustomLink from '../components/CustomLink';
+import { createClient } from 'contentful';
+import Badge from '../components/Badge';
 
-const About = () => {
+interface Technology {
+  title: string;
+  image_url: string;
+}
+
+interface Content {
+  fields: {
+    heading: string;
+    skills: {
+      frontend: {
+        js_frameworks: Technology[];
+        css_frameworks: Technology[];
+      };
+      build_dev_tools: Technology[];
+      cms: Technology[];
+      database_management: Technology[];
+      api_design_architectures: Technology[];
+      project_management: Technology[];
+      others: Technology[];
+    };
+  };
+}
+
+const About: React.FC<{ TECHNOLOGIES: Content[] }> = ({ TECHNOLOGIES }) => {
+  const { fields } = TECHNOLOGIES[0] || {};
+  const { heading, skills } = fields || {};
+  const {
+    frontend,
+    cms,
+    database_management,
+    api_design_architectures,
+    build_dev_tools,
+    project_management,
+    others,
+  } = skills;
+
   return (
     <>
       <Head>
@@ -54,15 +89,67 @@ const About = () => {
             <CustomHeading color="black" headingSup="Toolbox" headingSub=" My Weapons ⚔" />
 
             <h3
-              className="text-primary  text-2xl lg:text-4xl font-semibold mb-7 lg:mb-14"
+              className="text-primary text-2xl lg:text-4xl font-semibold mb-7 lg:mb-14"
               data-aos="fade-down"
               data-aos-duration="1000"
             >
-              Technologies that I use
+              {heading}
             </h3>
-            <div className="flex gap-5 lg:gap-7 flex-wrap justify-center items-center">
-              {SKILL_DATA.map((e) => (
-                <Circle data={e} key={e.id} />
+          </div>
+          <div className="container">
+            <h3 className="text-primary text-lg mb-4">CSS Frameworks</h3>
+            <div className="flex gap-5 flex-wrap items-center mb-8">
+              {frontend?.css_frameworks?.map((item: any, index: any) => (
+                <Badge data={item} key={index} />
+              ))}
+            </div>
+
+            <h3 className="text-primary text-lg mb-4">JS Frameworks</h3>
+            <div className="flex gap-5 flex-wrap items-center mb-8">
+              {frontend.js_frameworks.map((item: any, index: any) => (
+                <Badge data={item} key={index} />
+              ))}
+            </div>
+
+            <h3 className="text-primary text-lg mb-4">CMS</h3>
+            <div className="flex gap-5 flex-wrap items-center mb-8">
+              {cms.map((item: any, index: any) => (
+                <Badge data={item} key={index} />
+              ))}
+            </div>
+
+            <h3 className="text-primary text-lg mb-4">Build dev tools</h3>
+            <div className="flex gap-5 flex-wrap items-center mb-8">
+              {build_dev_tools.map((item: any, index: any) => (
+                <Badge data={item} key={index} />
+              ))}
+            </div>
+
+            <h3 className="text-primary text-lg mb-4">Api Design Architectures</h3>
+            <div className="flex gap-5 flex-wrap items-center mb-8">
+              {api_design_architectures.map((item: any, index: any) => (
+                <Badge data={item} key={index} />
+              ))}
+            </div>
+
+            <h3 className="text-primary text-lg mb-4">Api Design Architectures</h3>
+            <div className="flex gap-5 flex-wrap items-center mb-8">
+              {database_management.map((item: any, index: any) => (
+                <Badge data={item} key={index} />
+              ))}
+            </div>
+
+            <h3 className="text-primary text-lg mb-4">Api Design Architectures</h3>
+            <div className="flex gap-5 flex-wrap items-center mb-8">
+              {project_management.map((item: any, index: any) => (
+                <Badge data={item} key={index} />
+              ))}
+            </div>
+
+            <h3 className="text-primary text-lg mb-4">Project Management</h3>
+            <div className="flex gap-5 flex-wrap items-center mb-8">
+              {others.map((item: any, index: any) => (
+                <Badge data={item} key={index} />
               ))}
             </div>
           </div>
@@ -73,3 +160,26 @@ const About = () => {
 };
 
 export default About;
+
+export async function getStaticProps() {
+  const spaceId = process.env.CONTENTFUL_SPACE_ID;
+  const accessToken = process.env.CONTENTFUL_MANAGEMENT_TOKEN;
+
+  if (!spaceId || !accessToken) {
+    throw new Error('Contentful environment variables are not defined');
+  }
+
+  const client = createClient({
+    space: spaceId,
+    accessToken: accessToken,
+  });
+
+  const technologies = await client.getEntries({ content_type: 'technologies' });
+
+  return {
+    props: {
+      TECHNOLOGIES: technologies.items,
+    },
+    revalidate: 10,
+  };
+}
